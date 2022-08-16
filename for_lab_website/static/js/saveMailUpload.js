c077a27d9094d8518a7972a0512a08254a57ab95
arzou <amyzou@berkeley.edu>
2022-08-15 19:26:30 -0700

    Update and rename saveAndMail.js to saveMailUpload.js
    updated the original file with box upload functionality

--- for_lab_website/static/js/saveMailUpload.js
+++ for_lab_website/static/js/saveMailUpload.js
@@ -3,6 +3,7 @@ You will only have to change the three string variables outside the functions. *
 
 // URL of where the PHP files are located -- replace with whatever website/directory you end up using
  const save_url = "https://experiments-ccn.berkeley.edu/demoRLWM/save_data.php";
+ const upload_url = "https://experiments-ccn.berkeley.edu/demoRLWM/uploader.php";
  const mail_url = "https://experiments-ccn.berkeley.edu/demoRLWM/data/mailer.php";
 // name of folder where data will be saved
 const data_dir = "data";
@@ -21,6 +22,31 @@ const save_data_csv = function(file_name,toSave) {
     });
 }
 
+// mail data as csv
+const mail_data_csv = function(file_name) {
+  jQuery.ajax({
+        type: 'post',
+        cache: false,
+        url: mail_url,
+        data: {
+            file_name: file_name + '.csv',
+        }
+    });
+}
+
+// upload data as json
+const upload_data_csv = function(file_name) {
+  jQuery.ajax({
+    type: 'post',
+    cache: false,
+    url: upload_url,
+    data: {
+      data_dir: data_dir,
+      file_name: file_name + '.csv',
+    }
+  });
+}
+
 // save data as json
 const save_data_json = function(file_name,toSave) {
     jQuery.ajax({
@@ -35,18 +61,6 @@ const save_data_json = function(file_name,toSave) {
     });
 }
 
-// mail data as csv
-const mail_data_csv = function(file_name) {
-  jQuery.ajax({
-        type: 'post',
-        cache: false,
-        url: mail_url,
-        data: {
-            file_name: file_name + '.csv',
-        }
-    });
-}
-
 // mail data as json
 const mail_data_json = function(file_name) {
   jQuery.ajax({
@@ -57,4 +71,17 @@ const mail_data_json = function(file_name) {
             file_name: file_name + '.json',
         }
     });
+}
+
+// upload data as json
+const upload_data_json = function(file_name) {
+  jQuery.ajax({
+    type: 'post',
+    cache: false,
+    url: upload_url,
+    data: {
+      data_dir: data_dir,
+      file_name: file_name + '.json',
+    }
+  });
 }
